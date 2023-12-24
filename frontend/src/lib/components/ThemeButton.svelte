950d5cba52993450a02f08c4116eca068ca2e1a3
Eduardo Menges Mattje <eduardomattje@gmail.com>
2023-12-23 22:07:59 -0300

    Logic is working, but dropdown still don't

--- frontend/src/lib/components/ThemeButton.svelte
+++ frontend/src/lib/components/ThemeButton.svelte
@@ -1,39 +1,43 @@
 <script lang="ts">
-  enum Options {
-    Auto,
-    Light,
-    Dark,
+  interface ThemeOption {
+    name: string;
+    set(): void;
   }
 
-  let selected = Options.Auto;
+  const lightMode: ThemeOption = {
+    name: "☀️ Claro",
+    set() {
+      const doc = document.querySelector("html");
+      doc?.setAttribute("data-bs-theme", "light");
+    },
+  };
+
+  const darkMode: ThemeOption = {
+    name: "🌒 Escuro",
+    set() {
+      const doc = document.querySelector("html");
+      doc?.setAttribute("data-bs-theme", "dark");
+    },
+  };
+
+  const autoMode: ThemeOption = {
+    name: "🌗 Automático",
+    set() {
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        darkMode.set();
+      } else {
+        lightMode.set();
+      }
+    },
+  }
 </script>
 
 <div class="nav-item dropdown">
   <button class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown">
-    Clique.
-    <ul class="dropdown-menu">
-      <li><button class="dropdown-item" type="button">☀️ Claro</button></li>
-      <li><button class="dropdown-item" type="button">🌒 Escuro</button></li>
-      <li>
-        <button class="dropdown-item" type="button">🌗 Automático</button>
-      </li>
+    <ul class="dropdown-menu show">
+      {#each [lightMode, darkMode, autoMode] as opt}
+        <li><button class="dropdown-item" type="button" on:click={opt.set}>{opt.name}</button></li>
+      {/each}
     </ul>
   </button>
 </div>
-
-<div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    Dropdown
-  </button>
-  <ul class="dropdown-menu">
-    <li><button class="dropdown-item" type="button">Action</button></li>
-    <li><button class="dropdown-item" type="button">Another action</button></li>
-    <li><button class="dropdown-item" type="button">Something else here</button></li>
-  </ul>
-</div>
-
-<!-- 
-on:click={() => {
-  const doc = document.querySelector("html");
-  doc?.setAttribute("data-bs-theme", "light");
-}}>Click</button -->
