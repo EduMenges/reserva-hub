7b1d5aa08040893f0432285bac093c0c3a8e049c
Felipe Gallois <galloisfelipe@gmail.com>
2023-12-26 16:09:50 -0300

    Customize default spring project

--- backend/src/main/java/com/reservahub/backend/app/BackendApplication.java
+++ backend/src/main/java/com/reservahub/backend/app/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.reservahub.backend;
+package com.reservahub.backend.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
