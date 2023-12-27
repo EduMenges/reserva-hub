2ee6aba266aeaa272f04847b05fa695ccee7dd99
Eduardo Menges Mattje <eduardomattje@gmail.com>
2023-12-27 09:38:57 -0300

    Removed yarn

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-  import Logo from "$lib/components/Logo.svelte"
+  import Logo from "$lib/components/Logo.svelte";
 </script>
 
-<Logo/>
+<Logo />
 <p>Entre com a sua conta pessoal do INF</p>
 
 <form>
   <div class="mb-3">
-  <label class="form-label" for="username">Usuário</label>
-  <input id="username" class="form-control" type="text" />
-</div>
-<div class="mb-3">
-  <label class="form-label" for="password">Senha</label>
-  <input id="password" class="form-control" type="password" />
-</div>
+    <label class="form-label" for="username">Usuário</label>
+    <input id="username" class="form-control" type="text" />
+  </div>
+  <div class="mb-3">
+    <label class="form-label" for="password">Senha</label>
+    <input id="password" class="form-control" type="password" />
+  </div>
 
   <button type="submit" class="btn btn-primary">Login</button>
 </form>
