0d70ed6fa5bcb2d497b055ca7a9acd231697ab5e
Felipe Gallois <galloisfelipe@gmail.com>
2023-12-30 18:59:36 -0300

    Add a sql cript for startup

---
+++ backend/sql/startup.sql
@@ -0,0 +1,11 @@
+-- Senha: teste
+INSERT INTO users (username, password, role)
+VALUES ('student', '$2a$10$cRZ6dFxZTWOlgK68jm/HaOHz726q9L5Il9tS3/fkHxK.bzcROPOfu', 'STUDENT');
+
+-- Senha: teste
+INSERT INTO users (username, password, role)
+VALUES ('teacher', '$2a$10$cRZ6dFxZTWOlgK68jm/HaOHz726q9L5Il9tS3/fkHxK.bzcROPOfu', 'TEACHER');
+
+-- Senha: teste
+INSERT INTO users (username, password, role)
+VALUES ('admin', '$2a$10$cRZ6dFxZTWOlgK68jm/HaOHz726q9L5Il9tS3/fkHxK.bzcROPOfu', 'ADMIN');
