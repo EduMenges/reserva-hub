cd532268bacb1831475f6e239dbb78fcb9d055c6
Felipe Gallois <galloisfelipe@gmail.com>
2023-12-30 16:25:26 -0300

    Remove some unnecessary pieces of code

--- backend/src/main/java/com/reservahub/backend/app/auth/AuthRequest.java
+++ backend/src/main/java/com/reservahub/backend/app/auth/AuthRequest.java
@@ -2,11 +2,9 @@ package com.reservahub.backend.app.auth;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class AuthRequest {
     private String registration; 
     private String password;    
