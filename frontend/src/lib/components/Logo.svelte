dbd27f3dcfdfcdcdb59e5e538846b534258c451b
Eduardo Menges Mattje <eduardomattje@gmail.com>
2023-12-31 14:37:29 -0300

    Added class attribute to  Logo

--- frontend/src/lib/components/Logo.svelte
+++ frontend/src/lib/components/Logo.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   export let textClass = "text-primary";
   let className: string = "";
+  export { className as class };
 </script>
 
-<h1>Reserva<span class={`${textClass} ${className}`}>Hub</span></h1>
+<h1 class={className}>Reserva<span class={textClass}>Hub</span></h1>
