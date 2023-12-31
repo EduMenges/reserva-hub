a8ed2b2fc8f5bf159a196295609c31081104754c
Eduardo Menges Mattje <eduardomattje@gmail.com>
2023-12-31 14:36:57 -0300

    Added small container component

---
+++ frontend/src/lib/components/SmallContainer.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  let className = "";
+  export { className as class };
+
+  export let style = "";
+</script>
+
+<section class={`m-auto ${className}`} {style}>
+  <slot />
+</section>
+
+<style lang="scss">
+  @import "../../variables.scss";
+
+  section {
+    max-width: map-get($grid-breakpoints, sm) * 0.65;
+  }
+</style>
