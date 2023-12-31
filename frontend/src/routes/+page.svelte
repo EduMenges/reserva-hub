80d05ecf1a55dac5a64aa123c6579d0a396d1e73
Eduardo Menges Mattje <eduardomattje@gmail.com>
2023-12-31 14:43:48 -0300

    Using small container

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
   import Logo from "$lib/components/Logo.svelte";
+  import SmallContainer from "$lib/components/SmallContainer.svelte";
 </script>
 
-<Logo />
-<a href="book" class="btn btn-primary" role="button">Fazer uma reserva</a>
-<a href="history" class="btn btn-secondary" role="button">Suas reservas</a>
+<SmallContainer>
+  <Logo />
+  <div class="d-flex justify-content-between flex-wrap">
+    <a href="book" class="btn btn-primary" role="button">Fazer uma reserva</a>
+    <a href="history" class="btn btn-secondary" role="button">Suas reservas</a>
+  </div>
+</SmallContainer>
