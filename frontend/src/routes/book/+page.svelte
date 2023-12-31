25ee2759b4214f699b50361f1e3ca97a540a9570
Eduardo Menges Mattje <eduardomattje@gmail.com>
2023-12-31 15:26:21 -0300

    More minor fixes

--- frontend/src/routes/book/+page.svelte
+++ frontend/src/routes/book/+page.svelte
@@ -7,37 +7,43 @@
 </div>
 
 <form method="get" action="?/search">
-  <div class="mb-3">
-    <label for="date" class="form-label">Data</label>
-    <input type="date" class="form-control" name="date" id="date" min={currentDay()} required />
-  </div>
+  <fieldset name="horario">
+    <legend>Horário</legend>
 
-  <fieldset name="Horário" class="grid" style="row-gap: 1px">
-    <legend style="grid-row: 1;">Horário</legend>
+    <div class="row">
+      <div class="mb-3 col-12 col-sm-6">
+        <label for="date" class="form-label">Data</label>
+        <input type="date" class="form-control" name="date" id="date" min={currentDay()} required />
+      </div>
 
-    <div class="mb-3 g-col-6" style="grid-row: 2">
-      <label for="time-start" class="form-label">Início</label>
-      <input type="time" class="form-control" name="time-start" id="time-start" required />
-    </div>
+      <div class="mb-3 col">
+        <label for="time-start" class="form-label">Início</label>
+        <input type="time" class="form-control" name="time-start" id="time-start" required />
+      </div>
 
-    <div class="mb-3 g-col-6" style="grid-row: 2">
-      <label for="time-end" class="form-label">Fim</label>
-      <input type="time" class="form-control" name="time-end" id="time-end" required />
+      <div class="mb-3 col">
+        <label for="time-end" class="form-label">Fim</label>
+        <input type="time" class="form-control" name="time-end" id="time-end" required />
+      </div>
     </div>
   </fieldset>
 
-  <div class="mb-3">
-    <label for="title" class="form-label">Título do evento</label>
-    <input type="text" name="title" id="title" class="form-control" required />
-  </div>
+  <fieldset name="informacoes">
+    <legend>Informações</legend>
 
-  <div class="mb-3">
-    <label for="description" class="form-label">Descrição</label>
-    <textarea name="description" id="description" cols="30" rows="10" class="form-control" required></textarea>
-  </div>
+    <div class="mb-3">
+      <label for="title" class="form-label">Título do evento</label>
+      <input type="text" name="title" id="title" class="form-control" required />
+    </div>
+
+    <div class="mb-3">
+      <label for="description" class="form-label">Descrição</label>
+      <textarea name="description" id="description" cols="30" rows="10" class="form-control" required></textarea>
+    </div>
+  </fieldset>
 
   <button type="submit" class="btn btn-primary">
-    <i class="bi bi-search" />
+    <i class="bi bi-search me-2" />
     Buscar salas
   </button>
 </form>
