c58bcbc2ae39178f00f3c99fc51fd8cca22bc762
Felipe Gallois <galloisfelipe@gmail.com>
2024-01-04 15:52:52 -0300

    Make controller get user details from context holder

--- backend/src/main/java/com/reservahub/backend/app/reservationRequest/ReservationRequestController.java
+++ backend/src/main/java/com/reservahub/backend/app/reservationRequest/ReservationRequestController.java
@@ -2,7 +2,8 @@ package com.reservahub.backend.app.reservationRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,8 +20,10 @@ public class ReservationRequestController {
 
     @PostMapping("/send")
     public ResponseEntity<String> emitRequest(
-            @AuthenticationPrincipal UserDetails user,
             @RequestBody ReservationRequestDTO request) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails user = (UserDetails) authentication.getPrincipal();
+
         reservationRequestService.emitRequest(user, request);
 
         return ResponseEntity.ok("Reservation created.");
