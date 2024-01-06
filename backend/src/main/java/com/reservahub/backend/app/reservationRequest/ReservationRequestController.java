8e0216a44c070c785897a1789a86e3e197226fea
joaoPauloDias <joaopaulovasquezdias@gmail.com>
2024-01-05 23:04:09 -0300

    changing endpoint and adding userDetails by Authentication

--- backend/src/main/java/com/reservahub/backend/app/reservationRequest/ReservationRequestController.java
+++ backend/src/main/java/com/reservahub/backend/app/reservationRequest/ReservationRequestController.java
@@ -2,30 +2,28 @@ package com.reservahub.backend.app.reservationRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.reservahub.backend.app.reservationRequest.dto.ReservationRequestDto;
 import com.reservahub.backend.app.user.UserDetails;
 
 @RestController
-@RequestMapping("/request")
+@RequestMapping("/reservation-request")
 public class ReservationRequestController {
-    
+
     @Autowired
     ReservationRequestService reservationRequestService;
 
     @PostMapping("/send")
-    public ResponseEntity<String> emitRequest(
-            @RequestBody ReservationRequestDTO request) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        UserDetails user = (UserDetails) authentication.getPrincipal();
+    public ResponseEntity<ReservationRequest> emitRequest(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @RequestBody ReservationRequestDto request) {
+        ReservationRequest emitedRequest = reservationRequestService.emitRequest(userDetails, request);
 
-        reservationRequestService.emitRequest(user, request);
-
-        return ResponseEntity.ok("Reservation created.");
+        return ResponseEntity.ok(emitedRequest);
     }
 }
