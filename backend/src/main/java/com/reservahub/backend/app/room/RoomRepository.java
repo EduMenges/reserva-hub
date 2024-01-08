031e6423d4acdce4585809d08e8b322449bb37de
joaoPauloDias <joaopaulovasquezdias@gmail.com>
2024-01-07 22:02:33 -0300

    Add RoomRepository

--- backend/src/main/java/com/reservahub/backend/app/room/RoomRepository.java
+++ backend/src/main/java/com/reservahub/backend/app/room/RoomRepository.java
@@ -1,9 +1,10 @@
 package com.reservahub.backend.app.room;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RoomRepository extends JpaRepository<Room, Long> {
+public interface RoomRepository extends JpaRepository<Room, Long>, JpaSpecificationExecutor<Room> {
     
 }
