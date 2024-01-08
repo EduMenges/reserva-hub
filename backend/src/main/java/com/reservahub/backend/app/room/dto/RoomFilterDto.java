d6b92f0ed664d26489a809e29cdd246d9385a2ad
joaoPauloDias <joaopaulovasquezdias@gmail.com>
2024-01-07 22:02:14 -0300

    Adding RoomFilterDto

---
+++ backend/src/main/java/com/reservahub/backend/app/room/dto/RoomFilterDto.java
@@ -0,0 +1,19 @@
+package com.reservahub.backend.app.room.dto;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class RoomFilterDto {
+    private String roomNumber;
+    private String buildingNumber;
+    private String roomType;
+    private Short capacity;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private LocalDate date;
+    private List<String> resources;
+}
