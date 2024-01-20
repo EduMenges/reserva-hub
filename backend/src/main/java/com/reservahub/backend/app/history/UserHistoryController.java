f2b4c4743ec245f56bf1b08bb462b3b9c6c345a0
joaoPauloDias <joaopaulovasquezdias@gmail.com>
2024-01-19 23:45:38 -0300

    add admin endpoint for history

---
+++ backend/src/main/java/com/reservahub/backend/app/history/UserHistoryController.java
@@ -0,0 +1,38 @@
+package com.reservahub.backend.app.history;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.reservahub.backend.app.history.dto.UserHistoryEntryDTO;
+import com.reservahub.backend.app.user.User;
+import com.reservahub.backend.app.user.UserDetails;
+
+@RestController
+@RequestMapping("/history")
+public class UserHistoryController {
+
+    @Autowired
+    private UserHistoryService historyService;
+
+    @PreAuthorize("hasAuthority('STUDENT') or hasAuthority('TEACHER') or hasAuthority('ADMIN')")
+    @GetMapping("/get")
+    public ResponseEntity<ArrayList<UserHistoryEntryDTO>> getHistory(@AuthenticationPrincipal UserDetails userDetails) {
+        Long userId = userDetails.getId();
+
+        if (User.RoleEnum.TEACHER.name().equals(userDetails.getAuthorityName())) {
+            return ResponseEntity.ok(historyService.getTeacherHistory(userId));
+        } else if (User.RoleEnum.STUDENT.name().equals(userDetails.getAuthorityName())) {
+            return ResponseEntity.ok(historyService.getStudentHistory(userId));
+        }else {
+            return ResponseEntity.ok(historyService.getGlobalHistory());
+        }
+    }
+
+}
